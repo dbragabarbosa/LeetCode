b448a5055034aa054185d03c8539f35b3af531c5
Daniel Braga Barbosa <dbragabarbosa@gmail.com>
2025-09-06 18:48:23 -0300

    feat: add maxProfit solution

---
+++ LeetCode/LeetCode/MaxProfit.swift
@@ -0,0 +1,41 @@
+//
+//  LeetCode #[121] - [Best Time to Buy and Sell Stock]
+//  Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+//
+//  Resolução por Daniel Braga Barbosa
+//  https://github.com/dbragabarbosa/LeetCode
+//
+
+import Foundation
+
+// Descrição:
+/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+*/
+
+class Solution 
+{
+    func maxProfit(_ prices: [Int]) -> Int 
+    {
+        var min = Int.max
+        var max = 0
+
+        for price in prices
+        {
+            if price < min
+            {
+                min = price
+            }
+            else if price - min > max
+            {
+                max = price - min
+            }
+        }
+        
+        return max
+    }
+}
