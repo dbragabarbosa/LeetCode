7f43ed8beec62f8c5d2cc029b2e0a434bafcaacb
Daniel Braga Barbosa <dbragabarbosa@gmail.com>
2025-09-06 16:13:05 -0300

    feat: add reverse string solution

---
+++ ReverseString.swift
@@ -0,0 +1,35 @@
+//
+//  LeetCode #[344] - [Reverse String]
+//  Link: https://leetcode.com/problems/reverse-string
+//
+//  Resolução por Daniel Braga Barbosa
+//  https://github.com/dbragabarbosa/LeetCode
+//
+
+import Foundation
+
+// Descrição:
+/*
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+*/
+
+class Solution 
+{
+    func reverseString(_ s: inout [Character]) 
+    {
+        var start = 0
+        var end = s.count - 1
+
+        while start < end
+        {
+            var aux = s[start]
+            s[start] = s[end]
+            s[end] = aux
+
+            start += 1
+            end -= 1
+        }
+    }
+}
