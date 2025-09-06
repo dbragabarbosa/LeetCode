1ef9b5d79fe7f84d7d4cf9da9d02578cc815fda0
Daniel Braga Barbosa <dbragabarbosa@gmail.com>
2025-09-06 16:50:35 -0300

    feat: add isPalindrome solution

---
+++ isPalindrome.swift
@@ -0,0 +1,45 @@
+//
+//  LeetCode #[125] - [Valid Palindrome]
+//  Link: https://leetcode.com/problems/valid-palindrome
+//
+//  Resolução por Daniel Braga Barbosa
+//  https://github.com/dbragabarbosa/LeetCode
+//
+
+import Foundation
+
+// Descrição:
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+*/
+
+class Solution 
+{
+    func isPalindrome(_ s: String) -> Bool 
+    {
+        var sArray = Array(s.lowercased())
+
+        sArray = sArray.filter
+        {
+            $0.isLetter || $0.isNumber
+        }
+
+        var left = 0
+        var right = sArray.count - 1
+
+        while left < right
+        {
+            if sArray[left] != sArray[right]
+            {
+                return false
+            }
+
+            left += 1
+            right -= 1
+        }
+
+        return true
+    }
+}
